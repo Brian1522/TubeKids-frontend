17e500a66114e8e7aee3f983f522190cb169b960
Brian Laguna <66448870+Brian1522@users.noreply.github.com>
2024-03-18 23:54:00 -0600

    Adding authentication connect to backend

---
+++ authentication/authentication.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var loginButton = document.getElementById('loginButton');
+
+    loginButton.addEventListener('click', function () {
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+
+        if (!email || !password) {
+            alert("Por favor, ingrese un correo electrónico y una contraseña.");
+
+            return;
+        }
+        
+        fetch('http://localhost:8080/api/auth/login', {
+            method: "POST",
+            body: JSON.stringify({ email, password }),
+            headers:{"Content-Type": "application/json"},         
+        }).then(response => response.json()     
+        )
+        .then( data =>{
+            console.log(data)
+            
+            if ( data.user ){
+                localStorage.setItem("user",JSON.stringify(data.user))
+                window.location.replace("../accounts/accounts.html")
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Hubo un error al procesar su solicitud');
+        });
+    })
+        
+});
+
